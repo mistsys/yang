be6a5ca3b32f8ad89a35f0982a796a5c41bb4569
Mark Craig <mark@mistsys.com>
2020-10-07 21:16:36 -0700

    MIST-38552 - Adds python script and virtual environment to generate tree diagrams from Cisco YANG model files. Adds generated tree diagrams (*.tree.txt) for all Cisco IOS-XE versions. Cisco IOS-XR and NX not yet generated.

--- tools/visualization/gen_diagrams.sh
+++ tools/visualization/gen_diagrams.sh
@@ -1,66 +1,56 @@
 #!/bin/bash
 #
-# Script that generates the following for each YANG model...
-# 1) UML diagram (png image)
-# 2) UML diagram (svg image)
-# 3) Tree text diagram
-#
-# Mark Craig
-# mark@mist.com
+# Script that generates UML diagrams (png, svg) and text tree
+# diagrams for each Cisco YANG model. The Cisco YANG models are
+# segmented by OS type (NX-OS, IOS-XE, IOS-XR) and OS version
+# (ie: 16.12.1).
 #
 
 # get the script's directory (allow to be called from anywhere)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 TOP_DIR=$(cd $SCRIPT_DIR/../..; pwd)
+TOOLS_DIR=$SCRIPT_DIR/tools
+PLANTUML_DIR=$TOOLS_DIR/plantuml
 
 # global vars
-TOOLS=$TOP_DIR/tools
-IMAGES=$TOP_DIR/images
-VENDOR=$TOP_DIR/vendor
-PLANTUML_CMD="java -DPLANTUML_LIMIT_SIZE=8192 -jar ${TOOLS}/plantuml/plantuml.1.2018.5.jar -quiet"
-MODEL_DIRS=(
-    $VENDOR/cisco/xe/16101
+PLANTUML_CMD="java -DPLANTUML_LIMIT_SIZE=8192 -Djava.awt.headless=true -jar ${PLANTUML_DIR}/plantuml.1.2018.5.jar -quiet"
+VENDOR_DIR=$TOP_DIR/vendor
+OS_DIRS=(
+    $VENDOR_DIR/cisco/xe
+    #$VENDOR_DIR/cisco/nx
+    #$VENDOR_DIR/cisco/xr
 )
 
 # install dependencies
 echo "Installing python dependencies..."
-virtualenv $TOOLS/venv
-source $TOOLS/venv/bin/activate
+virtualenv $SCRIPT_DIR/venv
+source $SCRIPT_DIR/venv/bin/activate
 pip install -q -r $SCRIPT_DIR/requirements.txt
 
-# generate diagrams for each openconfig model
-for model_dir in "${MODEL_DIRS[@]}"; do
-    # create the model's image output directory (if needed)
-    IMAGES_OUT_DIR=$IMAGES/${model_dir#*$TOP_DIR/}
-    if [ ! -d $IMAGES_OUT_DIR ]; then
-        mkdir -p $IMAGES_OUT_DIR
-    fi
+# iterate over each os directory
+for os_dir in "${OS_DIRS[@]}"; do
+    # iterate over the list of version dirs in this os dir
+    find ${os_dir}/* -maxdepth 0 -type d | while read ver_dir; do
+        # iterate over the list of yang models in this os version dir
+        find ${ver_dir}/* -maxdepth 0 -type f -name "*.yang" | while read model; do
+            PARENT_DIR=$(dirname "$model")
+            NAME_NO_EXT=$(basename $model | sed -e 's/.yang//')
 
-    # for each yang model in the base directory
-    for model in `find $model_dir -type f -name "*.yang"`; do
-        # build the base output path
-        SHORT_PATH=${model#*$TOP_DIR/}
-        OUT_BASE_PATH=$IMAGES/${SHORT_PATH%.yang}
-        OUT_DIR=$IMAGES/$(dirname "${SHORT_PATH}")
+            # generate a tree text diagram for the model
+            TREE_DIAG=$PARENT_DIR/$NAME_NO_EXT.tree.txt
+            echo "Generating text tree diagram '$TREE_DIAG'..."
+            YANG_INSTALL=. pyang --path $PARENT_DIR -f tree --tree-print-groupings $model > $TREE_DIAG
 
-        # generate the uml text for the model
-        BASENAME=`basename $model | sed -e 's/.yang//'`
-        echo "Generating UML text for model '$BASENAME' model..."
-        INCLUDE_PATHS=$model_dir:$model_dir/MIBS:$model_dir/cbr:$model_dir/isr4k:$model_dir/cat9k
-        pyang --path $INCLUDE_PATHS -f uml $model -o $OUT_BASE_PATH.uml >/dev/null 2>&1
+            # generate the uml text for the model
+            #UML_FILE=$PARENT_DIR/$NAME_NO_EXT.uml
+            #echo "Generating UML text for YANG model '$NAME_NO_EXT'..."
+            #YANG_INSTALL=. pyang --path $PARENT_DIR -f uml $model -o $UML_FILE
 
-        # generate the uml diagram images
-        for ext in png svg; do
-            echo "Generating UML diagram '$BASENAME.$ext' for '$BASENAME' model..."
-            $PLANTUML_CMD $OUT_BASE_PATH.uml -t${ext} -o $OUT_DIR/
+            ## generate the uml diagram images
+            #for ext in png svg; do
+            #    echo "Generating UML diagram '$NAME_NO_EXT.$ext'..."
+            #    $PLANTUML_CMD $UML_FILE -t${ext} >/dev/null 2>&1
+            #done
         done
-
-        # generate a tree text diagram for the model
-        echo "Generating text tree '$BASENAME.tree.txt' for '$BASENAME' model..."
-        pyang --path $INCLUDE_PATHS -f tree $model > $OUT_BASE_PATH.tree.txt
-
-        # cleanup
-        mv $OUT_DIR/img/* $OUT_DIR/
-        rmdir $OUT_DIR/img
     done
 done
